9385d3f89c25e245001a277bddf61c9c3fef92c2
aquilaneo <aquilaneo@gmail.com>
2022-10-16 17:15:21 +0900

    TabViewコンポーネントでCheckbox作成

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
-<script setup lang="ts">
-</script>
-
 <template>
+	<TabView>
+		<TabContent>
 
+		</TabContent>
+
+		<TabContent>
+
+		</TabContent>
+	</TabView>
 </template>
 
 <style scoped>
 
 </style>
+
+<script setup lang="ts">
+import TabView from "@/components/TabView.vue";
+import TabContent from "@/components/TabContent.vue";
+</script>
---
+++ src/components/TabView.vue
@@ -0,0 +1,33 @@
+<template>
+	<div v-for="(tabContent, index) in tabContents">
+		<input type="radio" :id="index" :name="uuid">
+		<label :for="index">AAA</label>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script setup lang="ts">
+import {computed, ref, useSlots} from "vue";
+import {v4 as uuidv4} from "uuid";
+
+const uuid = ref (uuidv4 ());
+
+// TabContentたちを取得
+const tabContents = computed ((): string[] => {
+	// TabViewの子となる要素一覧を取得
+	const slots = useSlots ().default?. ();
+	if (!slots) {
+		return [];
+	}
+
+	const tabs = new Array<string> ();
+	for (const slot of slots) {
+		tabs.push ("AAA");
+	}
+
+	return tabs;
+});
+</script>
