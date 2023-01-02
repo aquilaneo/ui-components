267b749669b70d7e368b5fe5304d04ca02c0607c
aquilaneo <aquilaneo@gmail.com>
2023-01-02 17:52:59 +0900

    TabView完成

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
-	<TabView>
-		<TabContent label="タブ1" formName="tab-a">
-			<h1>タブ1の中身</h1>
-			<div>中身</div>
-		</TabContent>
+	<TabView :tabLabels="['AAA', 'BBB']">
+		<template #tab0>タブ0</template>
+		<template #tab1>タブ1</template>
+	</TabView>
 
-		<TabContent label="タブ2" formName="tab-a">
-			<h1>タブ2の中身</h1>
-			<div>中身</div>
-		</TabContent>
+	<TabView :tabLabels="['CCC', 'DDD']">
+		<template #tab0>タブA</template>
+		<template #tab1>タブB</template>
 	</TabView>
 </template>
 
@@ -18,5 +16,4 @@
 
 <script setup lang="ts">
 import TabView from "@/components/TabView.vue";
-import TabContent from "@/components/TabContent.vue";
 </script>
--- src/components/TabView.vue
+++ src/components/TabView.vue
@@ -1,11 +1,68 @@
 <template>
-	<slot/>
+	<div class="tabview">
+		<ul class="tabview-tab_container">
+			<li v-for="(tabLabel, index) in props.tabLabels"
+				class="tabview-tab" :class="{'tabview-tab_selected': index === selectedTab}">
+				<input type="radio" v-model="selectedTab" :name="uid" :value="index" :id="`${uid}-${index}`">
+				<label :for="`${uid}-${index}`">{{ tabLabel }}</label>
+			</li>
+		</ul>
+
+		<div v-for="(tabLabel, index) in props.tabLabels" class="tabview-content">
+			<slot :name="`tab${index}`" v-if="index === selectedTab"></slot>
+		</div>
+	</div>
 </template>
 
 <style scoped>
+ul {
+	padding: 0;
+	margin: 0;
+	display: flex;
+}
 
+li {
+	list-style: none;
+}
+
+input {
+	display: none;
+}
+
+label {
+	display: inline-block;
+	width: 100%;
+	height: 100%;
+}
 </style>
 
 <script setup lang="ts">
+import {onMounted, ref} from "vue";
+import {v4 as uuidv4} from "uuid";
 
+const props = defineProps ({
+	// タブのラベル配列
+	tabLabels: {
+		type: Array,
+		required: true
+	},
+	// 最初に選択されたタブ
+	initialTab: {
+		type: Number,
+		required: false
+	}
+});
+
+const selectedTab = ref (0); // 現在選択されているタブ
+const uid = ref (""); // inputタグのnameに使う一意なID
+
+onMounted (() => {
+	// 初期選択タブを反映する
+	if (props.initialTab) {
+		selectedTab.value = props.initialTab;
+	}
+
+	// 一意なIDを生成
+	uid.value = uuidv4 ();
+});
 </script>
